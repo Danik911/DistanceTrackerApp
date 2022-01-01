c2139ea8e7d4f663b81ae84dfca3bc3f9f34551c
Danik <porvipop@mail.ru>
2022-01-01 17:20:54 +0300

    Foreground service setup

--- app/src/main/java/com/danik/distancetrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/danik/distancetrackerapp/ui/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.danik.distancetrackerapp.util.Permissions.hasLocationPermission
 import com.danik.distancetrackerapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
